42e2f87a1aac009e5038c2ed88825a22732f96ca
ma <1390848128@qq.com>
2024-01-10 22:56:19 +0800

    19_Vue组件化编程_单文件组件

---
+++ 19_Vue组件化编程_单文件组件/Student.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- 组件的结构 -->
+    <div >
+      <h2>学生姓名：{{name}}</h2>
+      <h2>学生年龄：{{age}}</h2>
+  
+    </div>
+</template>
+<script>
+// 组件交互相关的代码（数据、方法等等）
+// ES6三种暴露方法详解https://blog.csdn.net/goutinga/article/details/109250386
+//分别暴露  
+//统一暴露
+//默认暴露
+export default {
+    name:'Student',
+    data() {
+      return {
+         name: '张三',
+         age: 18
+      }
+   },
+
+}
+
+</script>
+
+
+
